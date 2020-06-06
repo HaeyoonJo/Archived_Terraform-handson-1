2413cd942a97a0d171955ade46964097f3f31351
HaeyoonJo <jhys0226@gmail.com>
2020-06-07 02:03:29 +0300

    provision cognito user pool & app client

--- web-hosting/output.tf
+++ web-hosting/output.tf
@@ -1,3 +1,11 @@
 output "s3_website_endpoint" {
   value = aws_s3_bucket.unicorn.website_endpoint
 }
+
+output "cognito" {
+  value = aws_cognito_user_pool.pool.id
+}
+
+output "cognito_client" {
+  value = aws_cognito_user_pool_client.client.id
+}
