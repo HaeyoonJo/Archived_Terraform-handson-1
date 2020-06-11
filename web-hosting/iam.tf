70d2d69cd3e98492265e457414a13377ddfb2aad
HaeyoonJo <jhys0226@gmail.com>
2020-06-11 09:31:16 +0300

    modify given JSON value to file and provide the file path

--- web-hosting/iam.tf
+++ web-hosting/iam.tf
@@ -1,53 +1,33 @@
-resource "aws_iam_role" "role" {
-  name = "tf-LambdaExecRole"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+# create a role that accessable
+# to Lambda function and the dynamodb table specified
+# in the iam_role_policy_dynamodb.json file
+resource "aws_iam_role" "tf-lambda" {
+  name                = "tf-LambdaExecRole"
+  assume_role_policy  = file("policy_json/iam_assume_role_policy.json")
 
   tags = {
-    Name = "tf-iam-role-${var.tag_name}"
+    Name  = "iam-role-${var.tag_name}"
   }
 }
 
+# attach inline policy in order to attach created dynamodb my own
 resource "aws_iam_role_policy" "policy" {
-  name = "DynamoDBWriteAccess"
-  role = aws_iam_role.role.name
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": "dynamodb:PutItem",
-            "Resource": "arn:aws:dynamodb:eu-central-1:896457211177:table/tf-rides"
-        }
-    ]
-}
-EOF
+  name    = "DynamoDBWriteAccess"
+  role    = aws_iam_role.tf-lambda.name
+  policy  = file("policy_json/aws_iam_role_policy_dynamodb.json")
 }
 
+# attach AWSLambdaBasicExecutionRole policy in order to excute Lambda Function
 resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role        = aws_iam_role.tf-lambda.name
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-#  This resource isnt work in order to attach the dynamodbWrite policy. bc attaching is works on aws_iam_role_policy.policy resource.
-#  No need to write the attachment as below:
+
+# This resource isnt work in order to attach the dynamodbWrite policy,
+# bc attaching is works on aws_iam_role_policy.policy resource.
+# No need to write the attachment as below.
 # resource "aws_iam_role_policy_attachment" "test-attach1" {
-#   role       = aws_iam_role.role.name
+#   role       = aws_iam_role.tf-lambda.name
 #   policy_arn = aws_iam_role_policy.policy.name
 # }
