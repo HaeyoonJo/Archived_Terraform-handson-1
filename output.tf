1820c0f30a1ebd91b856eb6b212aba565aef7e34
HaeyoonJo <jhys0226@gmail.com>
2020-06-16 11:17:01 +0300

    change remote server dir

--- output.tf
+++
@@ -1,5 +0,0 @@
-# Output of Instance public IP
-output "public_ip" {
-  description = "List of public IP addresses assigned to the instances, if applicable"
-  value       = aws_instance.web.*.public_ip
-}
--- web-hosting/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.access_key_id
-  secret_key = var.secret_access_key
-}
